cd34c87d6bfc3f7c9dd92696ce242606f2e7c08b
Luigi <luigi.tanaka12@gmail.com>
2022-04-03 22:03:07 -0300

    js parte 2 e ajustes

--- scripts.js
+++ scripts.js
@@ -5,38 +5,58 @@ let sobremesa;
 
 function selecionarPrato(pratoSelecionado) {
     let jaSelecionado = document.querySelector(".pratos").querySelector(".borda-verde");
+    
     if (jaSelecionado !== null) {
         jaSelecionado.classList.remove("borda-verde");
+        jaSelecionado.querySelector(".icone").classList.add("esconder");
     }
+
     pratoSelecionado.classList.add("borda-verde");
+    pratoSelecionado.querySelector(".icone").classList.remove("esconder");
+
     prato = pratoSelecionado.innerHTML;
     habilitarBotao();
 }
 
 function selecionarBebida(bebidaSelecionada) {
     let jaSelecionado = document.querySelector(".bebidas").querySelector(".borda-verde");
+
     if (jaSelecionado !== null) {
         jaSelecionado.classList.remove("borda-verde");
+        jaSelecionado.querySelector(".icone").classList.add("esconder");
     }
+
     bebidaSelecionada.classList.add("borda-verde");
+    bebidaSelecionada.querySelector(".icone").classList.remove("esconder");
+
     bebida = bebidaSelecionada.innerHTML;
     habilitarBotao();
 }
 
 function selecionarSobremesa(sobremesaSelecionada) {
     let jaSelecionado = document.querySelector(".sobremesas").querySelector(".borda-verde");
+
     if (jaSelecionado !== null) {
         jaSelecionado.classList.remove("borda-verde");
+        jaSelecionado.querySelector(".icone").classList.add("esconder");
     }
+
     sobremesaSelecionada.classList.add("borda-verde");
+    sobremesaSelecionada.querySelector(".icone").classList.remove("esconder");
+
     sobremesa = sobremesaSelecionada.innerHTML;
     habilitarBotao();
 }
 
 function habilitarBotao () {
-    if (document.querySelector(".pratos").querySelector(".borda-verde") !== null && document.querySelector(".bebidas").querySelector(".borda-verde") !== null && document.querySelector(".sobremesas").querySelector(".borda-verde") !== null) {
+    let pratoJaSelecionado = document.querySelector(".pratos").querySelector(".borda-verde");
+    let bebidaJaSelecionada = document.querySelector(".bebidas").querySelector(".borda-verde");
+    let sobremesaJaSelecionada = document.querySelector(".sobremesas").querySelector(".borda-verde");
+    let botaoDeFinalizar = document.querySelector(".fechar-pedido button");
 
-        document.querySelector(".fechar-pedido button").classList.remove("desabilitado");
-        document.querySelector(".fechar-pedido button").classList.add("fundo-verde");
+    if ( pratoJaSelecionado !== null && bebidaJaSelecionada !== null && sobremesaJaSelecionada !== null) {
+        botaoDeFinalizar.classList.remove("desabilitado");
+        botaoDeFinalizar.classList.add("fundo-verde");
+        botaoDeFinalizar.innerHTML = `Fechar pedido`;
     }
 }
